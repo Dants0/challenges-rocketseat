511b5899c1edc6e537f7eed521efd8bb7816f5ba
Dants0 <gui.dg1@hotmail.com>
2022-05-29 13:10:40 -0300

    more functions

--- rocketcard/src/js/app.js
+++ rocketcard/src/js/app.js
@@ -1,7 +1,17 @@
 const colorBackground = document.querySelector('.container');
 const btnChangeColor = document.querySelector('.customCard');
+const resetColor = document.querySelector('.resetCard');
+
+const handleBackgroundColor = () =>{
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    colorBackground.style.backgroundColor = "#" + randomColor;
+}
+
+const resetBackgroundColor = () =>{
+    const originalColor = '#000000';
+    colorBackground.style.backgroundColor = originalColor;
+}
 
 
-btnChangeColor.addEventListener('click', () =>{
-    colorBackground.style.backgroundColor = 'rebeccapurple';
-})
+btnChangeColor.addEventListener('click', handleBackgroundColor);
+resetColor.addEventListener('click', resetBackgroundColor);
